a4851092b02061bb50e62deea921dc5ce528303e
Captimize <ssyth19@naver.com>
2018-05-09 14:10:48 +0900

    crawler

---
+++ temp.py
@@ -0,0 +1,147 @@
+import urllib
+import urllib.request
+import urllib.parse
+import bs4
+import re
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock 
+import codecs
+from konlpy.tag import Twitter
+
+def deleteTag(x):
+    return re.sub("<[^>]*>", "", x)
+
+def getMovieName(code):
+    def innerHTML(s, sl=0):
+        ret = ''
+        for i in s.contents[sl:]:
+            if i is str:
+                ret += i.strip()
+            else:
+                ret += str(i)
+        return ret
+ 
+    def fText(s):
+        if len(s): return innerHTML(s[0]).strip()
+        return ''
+    def rText(s):
+        if len(s): return (innerHTML(s[0]).strip() + innerHTML(s[1]).strip() + innerHTML(s[2]).strip() + innerHTML(s[3]).strip())
+    #print("여기까진 실행됨", code)
+    try:
+        _url = ("https://movie.naver.com/movie/bi/mi/basic.nhn?code=" + str(code))
+        #print("url : " , _url)
+        f = urllib.request.urlopen(_url)
+        #print("여기까진 실행됨")
+        data = f.read().decode('utf-8')   
+    except:
+        return
+    #print("여기도 실행댐")
+    #bs4를 이용해서 각각의 css 선택자를 통해 데이터를 긁어온다
+    soup = bs4.BeautifulSoup(re.sub("&#(?![0-9])", "", data),'html.parser')
+    director = fText(soup.select("#content > div > div.mv_info_area > div.mv_info > dl > dd > p > a")) 
+    name = fText(soup.select("#content > div.article > div.mv_info_area > div.mv_info > h3 > a"))
+    rateList = rText(soup.select("#pointNetizenPersentBasic > em"))
+    
+    print(rateList)
+    #print(name + " | " + director)
+    return name + "|" + director + "|" + rateList 
+
+def getComments(code):
+    def makeArgs(code, page):
+        params = {
+            'code': code,
+            'type': 'after',
+            'isActualPointWriteExecute': 'false',
+            'isMileageSubscriptionAlready': 'false',
+            'isMileageSubscriptionReject': 'false',
+            'page': page
+        }
+        return urllib.parse.urlencode(params)
+ 
+    def innerHTML(s, sl=0):
+        ret = ''
+        for i in s.contents[sl:]:
+            if i is str:
+                ret += i.strip()
+            else:
+                ret += str(i)
+        return ret
+ 
+    def fText(s):
+        if len(s): return innerHTML(s[0]).strip()
+        return ''
+ 
+    retList = []
+    colSet = set()
+    print("Processing: %d" % code)
+    page = 1
+    while 1:
+        try:
+            f = urllib.request.urlopen(
+                "http://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?" + makeArgs(code, page))
+            data = f.read().decode('utf-8')
+        except:
+            break
+        soup = bs4.BeautifulSoup(re.sub("&#(?![0-9])", "", data), 'html.parser')
+        cs = soup.select(".score_result li")
+        if not len(cs): break
+        for link in cs:
+            try:
+                url = link.select('.score_reple em a')[0].get('onclick')
+            except:
+                print(page)
+                print(data)
+                raise ""
+            m = re.search('[0-9]+', url)
+            if m:
+                url = m.group(0)
+            else:
+                url = ''
+            if url in colSet: return retList
+            colSet.add(url)
+            cat = fText(link.select('.star_score em'))
+            cont = fText(link.select('.score_reple p'))
+            cont = re.sub('<span [^>]+>.+?</span>', '', cont)
+            retList.append((url, cat, cont))
+        page += 1
+
+    return retList
+  
+def fetch(i):
+    outname = 'comments/%d.txt' % i
+    try:
+        if os.stat(outname).st_size > 0: return
+    except:
+        None
+    rs = getComments(i)
+    detail = getMovieName(i)
+    #print(rs[0])        
+    #print(detail)
+    if not len(rs): return
+
+    f = open(outname, 'w', encoding='utf-8')
+    
+    f.write("%s" %detail)
+    f.write('\n')
+    twitter = Twitter()
+    for idx, r in enumerate(rs):
+        if idx:
+            #각 사용자가 개인별로 부여한 평점
+            f.write("%s | " % (r[1]))
+            #koNLpy의 twitter 형태소 분석기를 통해 명사만 뽑아낸다.
+            malist = twitter.pos(r[2].replace("'", "''").replace("\\", "\\\\"),norm=True, stem=True)
+            word_dic = []
+            for word in malist:
+                if word[1] != "Josa" and word[1] != "Conjunction" and word[1] != "Punctuation":
+                    word_dic.append(word[0])
+            print(word_dic)
+            f.write("'%s';\n" %word_dic)           
+    f.close()
+    time.sleep(1)
+ 
+#5개의 쓰레드 생성
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for i in range(10001, 200000):
+        executor.submit(fetch, i)

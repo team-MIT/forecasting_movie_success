5139ba46ed080a9cc69f221962e250885e4b2d11
Juwon-Kim <Juwon-Kim@users.noreply.github.com>
2018-07-23 14:10:26 +0900

    Update imageUpdate.java

--- imageUpdate.java
+++ imageUpdate.java
@@ -116,3 +116,44 @@ public class FileName {
 	}
 
 }
+
+
+
+
+//  ========================================MovieListVO=============================================
+
+
+public class MovieListVO {
+	private int movie;
+	private String title;
+	private String director;
+	
+	
+	
+	public MovieListVO(int movie, String title, String director) {
+		super();
+		this.movie = movie;
+		this.title = title;
+		this.director = director;
+	}
+	public int getMovie() {
+		return movie;
+	}
+	public void setMovie(int movie) {
+		this.movie = movie;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	
+	
+}
